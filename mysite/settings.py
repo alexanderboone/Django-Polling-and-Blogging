be2bdda93295c2f65c5d9861cbf7495ff4624e6d
Alexander Boone <alboone@uw.edu>
2020-04-19 14:32:20 -0700

    added blogging app and model for a blog Post

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polling',
+    'blogging',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
